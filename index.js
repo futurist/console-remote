ce6e32c3688ae524782dc34749aaa01125ac945e
futurist <26634873@qq.com>
2018-05-26 16:03:11 +0800

    add now.json

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 const http = require('http')
 const fs = require('fs')
-const LOG = 'data.log'
-const PORT = process.env.PORT || process.env.DEFAULT_PORT || 16666
+const {parse} = require('url')
+
+require('dotenv').config()
+const {MongoClient} = require('mongodb')
+const HTTP_PORT = process.env.PORT || process.env.DEFAULT_PORT || 16666
+const {LOG_FILE, MONGO_URL, MONGO_DB} = process.env
 
 http.createServer((req, res)=>{
+  const {url} = req
+  const {query={}, pathname} = parse(url, true)
 
   if(req.method==='POST'){
-    const str=[Buffer.from('\n\n'+ req.url + ' ' + (new Date()) + '\n' )]
+    const date = new Date()
+    const str=[]
     req.on('data', data=>str.push(data))
     req.on('end', ()=>{
-      fs.appendFile(LOG, Buffer.concat(str), err=>console.log(err))
+      const data = Buffer.concat(str).toString('utf8')
+      if(LOG_FILE) fs.appendFile(
+        LOG_FILE,
+        '\n\n'+ url + ' ' + date + '\n' + data,
+        err=>console.log(err)
+      )
+      if(saveToMongo) saveToMongo({
+        data, date, url
+      }, (err)=>console.log(err))
     })
     res.writeHead(200, {
       'Access-Control-Allow-Origin': '*',
@@ -17,15 +32,47 @@ http.createServer((req, res)=>{
     })
   }
 
-  if(req.url=='/view'){
+  if(pathname=='/view'){
+    console.log(query)
     res.writeHead(200, {
       'Content-Type': 'text/plain',
     })
-    const data = fs.createReadStream(LOG)
-    data.on('error', err=>res.end('no data'))
-    data.pipe(res)
+    if(LOG_FILE){
+      const data = fs.createReadStream(LOG_FILE)
+      data.on('error', err=>res.end('no data'))
+      data.pipe(res)
+    }
+    if(readFromMongo) {
+      let q = evalExpression(query.query)
+      console.log('query',q)
+      readFromMongo(q || [], (err,cursor)=>{
+        if(err) return console.log(err)
+        cursor.toArray((err, docs)=>{
+          if(err) return console.log(err)
+          res.end(JSON.stringify(docs))
+        })
+      })
+    }
   } else {
     res.end()
   }
-}).listen(PORT)
+}).listen(HTTP_PORT)
 
+var saveToMongo
+var readFromMongo
+console.log(MONGO_URL+MONGO_DB)
+MongoClient.connect(MONGO_URL+MONGO_DB, function(err, client) {
+  if(err) return console.log(err)
+  console.log('connected to mongodb')
+  const db = client.db(MONGO_DB)
+  saveToMongo = (data, cb)=>
+    db.collection('logs').insertOne(data, cb)
+  readFromMongo = (query, cb)=>
+    db.collection('logs').aggregate(query, cb)
+})
+
+function evalExpression(str){
+  try{
+    return new Function('return ('+str+')')()
+  }catch(e){}
+}
